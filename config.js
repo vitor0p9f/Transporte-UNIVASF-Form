19192259d37b818c61ca836b428f63717a34aed2
sansaocarrasco <joao.pedro_jua@hotmail.com>
2026-03-05 19:44:44 -0300

    create: db_integration

---
+++ config.js
@@ -0,0 +1,5 @@
+// Substitua os valores abaixo pelas credenciais do seu projeto no Supabase
+// Você pode encontrar esses valores no painel do Supabase em: Project Settings -> API
+
+const SUPABASE_URL = 'https://ajmfxwyjirwcukwqxtng.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_Wjm2Vs8LWZ0JiglqKiDSqA_WkjRHxe7';
